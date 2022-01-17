14030ce93b4364958782d08c917792ccb2efe2c1
jacob <gwhong917@gmail.com>
2022-01-17 18:36:42 +0900

    ㅁㄹㅇㄴ

--- parksite/urls.py
+++ parksite/urls.py
@@ -8,6 +8,14 @@ urlpatterns = [
     path('charts', views.charts),
     path('layout-static', views.ls),
     # path('index/', views.index, name='index'),
+    path('layout-sidenav-light', views.lsl),
+    path('401', views.h401),
+    path('404', views.h404),
+    path('500', views.h500),
+    path('login', views.login),
+    path('password', views.password),
+    path('register', views.register),
+    path('tables', views.tables)
 
 ]
 
--- parksite/views.py
+++ parksite/views.py
@@ -5,6 +5,9 @@ from django.http import HttpResponse
 # def index(request):
 #     return render(request, "index.html")
 
+def index(request):
+    return render(request, "index.html")
+
 
 def charts(request):
     return render(request, "charts.html")
@@ -14,9 +17,35 @@ def ls(request):
     return render(request, "layout-static.html")
 
 
-def index(req):
-    context = {
+def lsl(request):
+    return render(request, "layout-sidenav-light.html")
 
-    }
 
-    return render(req, "index.html", context=context)
+def h401(request):
+    return render(request, "401.html")
+
+
+def h404(request):
+    return render(request, "404.html")
+
+
+def h500(request):
+    return render(request, "500.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def password(request):
+    return render(request, "password.html")
+
+
+def register(request):
+    return render(request, "register.html")
+
+
+def tables(request):
+    return render(request, "register.html")
+
+
