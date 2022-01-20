b9d7eb5b3382628b2bb977f94cfd874539f5af2b
jacob <gwhong917@gmail.com>
2022-01-20 16:50:06 +0900

    ㅁㄴㅇㄹ

--- parksite/urls.py
+++ parksite/urls.py
@@ -15,7 +15,14 @@ urlpatterns = [
     path('login', views.login),
     path('password', views.password),
     path('register', views.register),
-    path('tables', views.tables)
-
+    path('tables', views.tables),
+    path('charts_1', views.charts_1),
+    path('charts_2', views.charts_2),
+    path('charts_3', views.charts_3),
+    path('charts_4', views.charts_4),
+    path('tables_area1', views.tables_area1),
+    path('tables_area2', views.tables_area2),
+    path('tables_time1', views.tables_time1),
+    path('tables_time2', views.tables_time2),
 ]
 
--- parksite/views.py
+++ parksite/views.py
@@ -49,3 +49,37 @@ def tables(request):
     return render(request, "tables.html")
 
 
+def charts_1(request):
+    return render(request, "charts_1.html")
+
+
+def charts_2(request):
+    return render(request, "charts_2.html")
+
+
+def charts_3(request):
+    return render(request, "charts_3.html")
+
+
+def charts_4(request):
+    return render(request, "charts_4.html")
+
+
+def tables_area1(request):
+    return render(request, "tables_area1.html")
+
+
+def tables_area2(request):
+    return render(request, "tables_area2.html")
+
+
+def tables_time1(request):
+    return render(request, "tables_time1.html")
+
+
+def tables_time2(request):
+    return render(request, "tables_time2.html")
+
+
+
+
